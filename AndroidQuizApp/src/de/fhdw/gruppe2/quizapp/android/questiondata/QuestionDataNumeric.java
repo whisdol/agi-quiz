7e70f9bb302ca4ab53dd26a7d4bdf0608ac00bdd
Cedric Lueke <cedric.lueke@gmail.com>
2015-01-07 18:33:41 +0100

    Create class for questions with numeric answers (question 4)

---
+++ AndroidQuizApp/src/de/fhdw/gruppe2/quizapp/android/questiondata/QuestionDataNumeric.java
@@ -0,0 +1,34 @@
+package de.fhdw.gruppe2.quizapp.android.questiondata;
+
+import java.util.List;
+
+public class QuestionDataNumeric extends QuestionData {
+	
+	private float mDeviation;
+	private boolean mDeviationIsPercent;
+
+	public QuestionDataNumeric(String pQuestion, List<String> pAnswers,
+			int pCorrectAnswer, float pDeviation, boolean pDeviationIsPercent) {
+		super(pQuestion, pAnswers, pCorrectAnswer);
+		mDeviation = pDeviation;
+		mDeviationIsPercent = pDeviationIsPercent;
+		
+	}
+
+	@Override
+	public boolean isCorrectAnswer(int pAnswer) {
+		int minValue;
+		int maxValue;
+		
+		if (mDeviationIsPercent){
+			minValue = (int) Math.floor(super.getCorrectAnswer()  * (1 - mDeviation));
+			maxValue = (int) Math.ceil(super.getCorrectAnswer() * (1 + mDeviation));
+		} else {
+			minValue = super.getCorrectAnswer() - Math.round(mDeviation);
+			maxValue = super.getCorrectAnswer() + Math.round(mDeviation);	
+		}
+		
+		return (pAnswer >= minValue && pAnswer <=maxValue);
+	}
+
+}
